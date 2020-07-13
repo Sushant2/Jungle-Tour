3466e816910e6a01a7e134fd706b8bcee687136e
Sushant2 <er.sushant.2@gmail.com>
2020-07-14 00:35:23 +0530

    added users count counter

--- script.js
+++ script.js
@@ -26,5 +26,33 @@ function closeForm(){
     $("#register-side-bar").css("z-index", "0");
     $("#call-num").css("opacity", "1");
     $("#reg").css("opacity", "1");
-
 }
+
+
+(function manageTotalUsers() {
+    
+    let totalUsers = String(getRandom(100000, 200000));
+    totalUsers = addComma(totalUsers);
+    
+    addToTotalUsers(totalUsers);
+    
+    function addToTotalUsers(currentValue) {
+        let num = Number(currentValue.replace(",", ""));
+        num++;
+        num = String(num);
+        num = addComma(num);
+        $("#total-users").html(num);
+        setTimeout(function() {
+            addToTotalUsers(num);
+        }, getRandom(100, 2000));
+    }
+    
+    function getRandom(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    
+    function addComma(num) {
+        return num.slice(0, 3) + "," + num.slice(3, 6);
+    }
+    
+})();
